5dd82d6080cbb2ccd97a8922745f257b9aaef07d
infinite-Zh <zhxuwork@163.com>
2018-02-12 23:46:07 +0800

    todo

--- app/src/main/java/com/infinite/androidtest/repository/LoginDataSource.kt
+++ app/src/main/java/com/infinite/androidtest/repository/LoginDataSource.kt
@@ -1,12 +1,20 @@
 package com.infinite.androidtest.repository
 
 import android.support.v4.content.ContextCompat
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 
 /**
  * Created by kfzhangxu on 2018/1/31.
  */
 class LoginDataSource : ILoginDataSource {
     override fun login(email: String, password: String, callBack: ILoginDataSource.LoginCallBack) {
+
+        // TODO() multi thread
+
+        Thread()
         if (email != "abc@163.com") {
             callBack.callBack(false, "用户名错误")
             return
@@ -15,7 +23,10 @@ class LoginDataSource : ILoginDataSource {
             callBack.callBack(false, "密码错误")
             return
         }
+
         callBack.callBack(true, "登陆成功")
     }
+
+
 
 }
