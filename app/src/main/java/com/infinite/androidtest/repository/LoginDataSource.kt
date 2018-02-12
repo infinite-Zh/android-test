90e9b6fa409d1d14aab82fc1d7d1e455f3d04213
infinite-Zh <zhxuwork@163.com>
2018-02-13 00:17:53 +0800

    changes

--- app/src/main/java/com/infinite/androidtest/repository/LoginDataSource.kt
+++ app/src/main/java/com/infinite/androidtest/repository/LoginDataSource.kt
@@ -1,5 +1,11 @@
 package com.infinite.androidtest.repository
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+
 /**
  * Created by kfzhangxu on 2018/1/31.
  */
@@ -8,19 +14,61 @@ class LoginDataSource : ILoginDataSource {
 
         // TODO() multi thread
 
-        Thread()
-        if (email != "abc@163.com") {
-            callBack.callBack(false, "用户名错误")
-            return
-        }
-        if (password != "123abc") {
-            callBack.callBack(false, "密码错误")
-            return
-        }
+        object : Thread() {
+            override fun run() {
+                super.run()
+                Thread.sleep(2000)
+                val handler=MyHandler(callBack)
+                val message=handler.obtainMessage()
+                var result:Boolean
+                var msg:String
+                val bundle=Bundle()
+                if (email != "abc@163.com") {
+//                    callBack.callBack(false, "用户名错误")
+                    result=false
+                    msg="用户名错误"
+                    bundle.putBoolean("result",result)
+                    bundle.putString("msg",msg)
+                    message.data=bundle
+                    handler.sendMessage(message)
+                    return
+                }
+                if (password != "123abc") {
+//                    callBack.callBack(false, "密码错误")
+                    result=false
+                    msg="密码错误"
+                    bundle.putBoolean("result",result)
+                    bundle.putString("msg",msg)
+                    message.data=bundle
+                    handler.sendMessage(message)
+                    return
+                }
 
-        callBack.callBack(true, "登陆成功")
+                result=true
+                msg="登陆成功"
+                bundle.putBoolean("result",result)
+                bundle.putString("msg",msg)
+                message.data=bundle
+                handler.sendMessage(message)
+//                callBack.callBack(true, "登陆成功")
+            }
+        }.start()
+
     }
 
+    class MyHandler() : Handler() {
+        private lateinit var callback: ILoginDataSource.LoginCallBack
 
+        constructor(callBack: ILoginDataSource.LoginCallBack) : this() {
+            this.callback = callBack
+        }
 
+        override fun handleMessage(msg: Message?) {
+            super.handleMessage(msg)
+            val b=msg!!.data
+            val result=b.getBoolean("result")
+            val msg=b.getString("msg")
+            callback!!.callBack(result,msg)
+        }
+    }
 }
