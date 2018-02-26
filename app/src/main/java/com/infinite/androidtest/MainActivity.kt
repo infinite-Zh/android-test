2e41cadf93cb76310fe7747e8bf5faedeb801f07
infinite-Zh <zhxuwork@163.com>
2018-02-26 14:32:28 +0800

    ADD MENU

--- app/src/main/java/com/infinite/androidtest/MainActivity.kt
+++ app/src/main/java/com/infinite/androidtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.infinite.androidtest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 
 /**
  * Created by kfzhangxu on 2018/1/31.
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
     }
 }
